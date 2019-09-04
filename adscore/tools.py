ef6dfb7b7668851551ea7a4c7bcfab4dddbc838e
Sergi Blanco-Cuaresma <marblestation@users.noreply.github.com>
2019-09-04 19:16:57 -0400

    Updated adsmutils to v1.1.2 + bugfix date parsing

--- adscore/tools.py
+++ adscore/tools.py
@@ -1,7 +1,10 @@
 from datetime import datetime
 
 def is_expired(auth):
-    expire_in = datetime.strptime(auth['expire_in'], "%Y-%m-%dT%H:%M:%S.%f")
+    try:
+        expire_in = datetime.strptime(auth['expire_in'], "%Y-%m-%dT%H:%M:%S")
+    except:
+        expire_in = datetime.strptime(auth['expire_in'], "%Y-%m-%dT%H:%M:%S.%f")
     delta = expire_in - datetime.now()
     return delta.total_seconds() < 0
 
